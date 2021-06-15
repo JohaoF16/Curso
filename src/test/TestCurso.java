9c751b51bfc5404c50551dfdf9887d9bf835f46b
steven flores <floressteven105@gmail.com>
2021-06-14 20:47:35 -0500

    Metodos setPromedio y setEstudiantesEncimaPromedio

---
+++ src/test/TestCurso.java
@@ -0,0 +1,24 @@
+
+package test;
+
+import dominio.Curso;
+
+public class TestCurso {
+    public static void main(String[] args) {
+        Curso cursoA = new Curso();
+        double [] notas = new double[12];
+        llenaNotas(notas);    
+        cursoA.setNotas(notas);
+        cursoA.mostrarNotas();
+        cursoA.setPromedio();
+        System.out.println("El Promedio del Curso: "+cursoA.getPromedio());
+        cursoA.setEstudiantesEncimaPromedio();
+        System.out.println("Numero de Estudiantes encima del Promedio: "+cursoA.getnroEstudiantesEncimaPromedio());
+
+    }
+    public static void llenaNotas(double [] notas){
+        for(int i=0;i<12;i++){
+            notas[i] = Math.random()*6;
+        }
+    }
+}
